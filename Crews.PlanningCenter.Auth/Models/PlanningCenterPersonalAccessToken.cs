c7bb8be24f6ee8f9fb85fe0aa77f3f57b0c8b5b8
Tommy Crews <twcrews96@gmail.com>
2025-08-21 08:49:36 -0500

    Add more scaffolding.

---
+++ Crews.PlanningCenter.Auth/Models/PlanningCenterPersonalAccessToken.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Headers;
+using Crews.Extensions.Primitives;
+
+namespace Crews.PlanningCenter.Auth.Models;
+
+/// <summary>
+/// Represents a developer personal access token for the Planning Center API.
+/// </summary>
+public readonly struct PlanningCenterPersonalAccessToken
+{
+    /// <summary>
+    /// The App ID component of the personal access token.
+    /// </summary>
+    public required string AppID { get; init; }
+
+    /// <summary>
+    /// The Secret component of the personal access token.
+    /// </summary>
+    public required string Secret { get; init; }
+
+    /// <summary>
+    /// Implicitly converts the current <see cref="PlanningCenterPersonalAccessToken"/> instance to an
+    /// <see cref="AuthenticationHeaderValue"/> instance.
+    /// </summary>
+    /// <param name="token">The current instance.</param>
+    public static implicit operator AuthenticationHeaderValue(PlanningCenterPersonalAccessToken token)
+        => new("Basic", $"{token.AppID}:{token.Secret}".Base64Encode());
+}
